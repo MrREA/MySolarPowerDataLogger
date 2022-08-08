f19847916326408cc0d552e6bbe51709d068af8e
wohlrata <andreas.wohlrath@roche.com>
2022-08-08 09:31:19 +0200

    Analog Read test

---
+++ ReadAnalogVoltage/ReadAnalogVoltage.ino
@@ -0,0 +1,32 @@
+/*
+  ReadAnalogVoltage
+
+  Reads an analog input on pin 0, converts it to voltage, and prints the result to the Serial Monitor.
+  Graphical representation is available using Serial Plotter (Tools > Serial Plotter menu).
+  Attach the center pin of a potentiometer to pin A0, and the outside pins to +5V and ground.
+
+  This example code is in the public domain.
+
+  http://www.arduino.cc/en/Tutorial/ReadAnalogVoltage
+*/
+
+// Global Variables
+int ADVoltage = 3300; // in mVolt
+int ADResolution = 12; // in Bit
+
+// the setup routine runs once when you press reset:
+void setup() {
+  // initialize serial communication at 9600 bits per second:
+  Serial.begin(115200);
+}
+
+// the loop routine runs over and over again forever:
+void loop() {
+  // read the input on analog pin 0:
+  int sensorValue = analogRead(A0);
+  // Convert the analog reading (which goes from 0 - 1023) to a voltage (0 - 5V):
+  //float voltage = sensorValue * (ADVoltage /(2^ADResolution));
+  float voltage = sensorValue * 0.8;
+  // print out the value you read:
+  Serial.println(voltage);
+}
